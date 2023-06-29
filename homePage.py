de48cbcdbda6e385c9601f9af388530bed0cbcfe
Jasintumba <67004014+Jasintumba@users.noreply.github.com>
2023-06-29 21:13:11 +0200

    Add files via upload

---
+++ homePage.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+# Define the dictionary of usernames and passwords
+USER_CREDS = {
+    "TamaraB": "Tamara190316",
+    "ZwPhiriJa": "Malaya1996",
+    "ZwMunengamiTa": "BossTwani",
+    "ZwMapfazFa": "FariMapfaza",
+    "ZwNdunaGi": "Induna2023"
+}
+
+def login():
+    """
+    Displays the login page and prompts the user to enter their
+    username and password. If the login is successful, the function
+    returns True, otherwise it returns False.
+    """
+    st.title("Welcome to Nestle Online Manufacturing System")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username in USER_CREDS and USER_CREDS[username] == password:
+            st.success("Logged in!")
+            
+
+
+st.set_page_config(
+page_title="Multipage App",
+page_icon="👋",
+)
+
+st.title("Nestle Online Processing Dashboard")
+st.sidebar.success("Select Specific Processing Plant above.")
+
+st.write("Note: This application is for authorized personnel only.")
+
+st.subheader(f'CURRENT PROCESSING PARAMETERS')
+st.write('Mono Pump Speed:', 35)
+
+st.write('Mono Pump Flowrate:', 2360)
+
+st.write('DSI Valve Opening:', 45)
+
+st.write('DSI 1 Temperature:', 75)
+
+st.write('Hydrolisis Waukesha Pump speed:', 85)
+
+st.write('DSI 2 Valve opening:', 61)
+
+st.write('DSI  2 Temperature:', 134)
+
+st.write('CCP 1 Temperature:', 134)
+
+st.write('CCP 2 Temperature:', 131)
+
+st.write('Flash Tank Waukesha Speed:', 100)
